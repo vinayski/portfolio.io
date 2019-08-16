57cdc27bbb2a52b4aefb270fe13315dd969fe1d7
vinayski <ski@sankhe.com>
2019-08-15 19:35:38 -0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,14 +33,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>portfolio</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Contact me to learn how to speed up and improve your overall performance.</h1>
